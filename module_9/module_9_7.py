e0a0820acfbe086ebb957b7100dd853bdd701a77
andrei-mortl <mor.tl08201988@gmail.com>
2024-12-24 16:56:18 +0300

    Add files via upload

--- module_9/module_9_7.py
+++ module_9/module_9_7.py
@@ -1,13 +1,19 @@
 def is_prime(func):
     def wrapper(*args):
-        num = func(*args)
-        for i in range(2, int(num ** 0.5) + 1):
-            if num % i == 0:
-                res = 'Составное'
+        n = func(*args)
+        is_prime = True
+        if n < 2:
+            return
+        else:
+            for j in range(2, int(n ** 0.5) + 1):
+                if n % j == 0:
+                    is_prime = False
+                    break
+            if is_prime:
+                print('Простое')
             else:
-                res =  'Простое'
-            print(res)
-            return num
+                print('Cоставное')
+            return n
     return wrapper
 
 @is_prime
